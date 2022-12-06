986572ff1704ea2c510a965baf7caf87aa915600
marfpss <69798927+mbbrpiuoficial@users.noreply.github.com>
2022-12-06 15:04:36 -0300

    making the site lighter

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const menu = document.querySelector('.menu');
-const NavMenu = document.querySelector('.nav-menu');
-
-menu.addEventListener('click', () => {
-    menu.classList.toggle('ativo');
-    NavMenu.classList.toggle('ativo');
-})
+const menu=document.querySelector('.menu');const NavMenu=document.querySelector('.nav-menu');menu.addEventListener('click',()=>{menu.classList.toggle('ativo');NavMenu.classList.toggle('ativo')})
